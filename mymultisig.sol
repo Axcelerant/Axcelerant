fbe5dc136c96e99fdc4ab319f7ddb44811b9c50f
Axcelerant <112372875+Axcelerant@users.noreply.github.com>
2022-09-01 18:23:20 +0300

    Add files via upload

---
+++ mymultisig.sol
@@ -0,0 +1,11 @@
+//SPDX-License-Udentifier: MIT
+pragma solidity ^0.8.10;
+
+
+contract MultiSigWallet {
+    event Deposit(address indexed sender, uint amount);
+    event Submit(uint indexed txId);
+    event A
+    
+    
+}
